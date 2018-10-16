619f6cf4bf0f7ece658956ef4090511209b235f4
Switch2Logic <switch2logic@switch2logic.rebelchain.co.za>
2018-10-16 18:52:16 +0200

    docker-compose-local

--- generate.sh
+++ generate.sh
@@ -6,7 +6,7 @@
 #
 export PATH=$GOPATH/src/github.com/hyperledger/fabric/build/bin:${PWD}/../bin:${PWD}:$PATH
 export FABRIC_CFG_PATH=${PWD}
-CHANNEL_NAME=Comunity_channel
+CHANNEL_NAME=comunitychannel
 
 # remove previous crypto material and config transactions
 rm -fr channel-artifacts/*
@@ -20,14 +20,14 @@ if [ "$?" -ne 0 ]; then
 fi
 
 # generate genesis block for orderer
-configtxgen -profile SingleOrdererHospitalGenesis -outputBlock ./channel-artifacts/genesis.block
+configtxgen -profile SingleOrdererHospitalGenesis -outputBlock ./channel-artifacts/genesis.block -channelID $CHANNEL_NAME
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate orderer genesis block..."
   exit 1
 fi
 
 # generate channel configuration transaction
-configtxgen -profile Comunity_channel -outputCreateChannelTx ./channel-artifacts/comunity_channel.tx -channelID $CHANNEL_NAME
+configtxgen -profile Comunitychannel -outputCreateChannelTx ./channel-artifacts/comunity_channel.tx -channelID $CHANNEL_NAME
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate channel configuration transaction..."
   exit 1
@@ -37,21 +37,21 @@ fi
 #------------------------------------------------------------------------------------------
 
 # generate anchor peer transaction
-configtxgen -profile Comunity_channel -outputAnchorPeersUpdate ./channel-artifacts/Hospital1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Hospital1MSP
+configtxgen -profile Comunitychannel -outputAnchorPeersUpdate ./channel-artifacts/Hospital1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Hospital1MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for Hospital1MSP..."
   exit 1
 fi
 
 # generate anchor peer transaction
-configtxgen -profile Comunity_channel -outputAnchorPeersUpdate ./channel-artifacts/Hospital2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Hospital2MSP
+configtxgen -profile Comunitychannel -outputAnchorPeersUpdate ./channel-artifacts/Hospital2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Hospital2MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for Hospital1MSP..."
   exit 1
 fi
 
 # generate anchor peer transaction
-configtxgen -profile Comunity_channel -outputAnchorPeersUpdate ./channel-artifacts/Hospital3MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Hospital3MSP
+configtxgen -profile Comunitychannel -outputAnchorPeersUpdate ./channel-artifacts/Hospital3MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Hospital3MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for Hospital1MSP..."
   exit 1
@@ -62,20 +62,20 @@ fi
 # Hospital1_2Channel generation
 #------------------------------------------------------------------------------------------
 # generate channel configuration transaction
-configtxgen -profile Hospital1_2_channel -outputCreateChannelTx ./channel-artifacts/hospital1_2_channel.tx -channelID Hospital1_2_channel
+configtxgen -profile Hospital2channel -outputCreateChannelTx ./channel-artifacts/hospital1_2_channel.tx -channelID Hospital2channel
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate channel configuration transaction..."
   exit 1
 fi
 # generate anchor peer transaction
-configtxgen -profile Hospital1_2_channel -outputAnchorPeersUpdate ./channel-artifacts/Hospital1MSPanchors.tx -channelID Hospital1_2_channel -asOrg Hospital1MSP
+configtxgen -profile Hospital2channel -outputAnchorPeersUpdate ./channel-artifacts/Hospital1_2MSPanchors.tx -channelID Hospital2channel -asOrg Hospital1MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for Hospital1MSP..."
   exit 1
 fi
 
 # generate anchor peer transaction
-configtxgen -profile Hospital1_2_channel -outputAnchorPeersUpdate ./channel-artifacts/Hospital2MSPanchors.tx -channelID Hospital1_2_channel -asOrg Hospital2MSP
+configtxgen -profile Hospital2channel -outputAnchorPeersUpdate ./channel-artifacts/Hospital2_1MSPanchors.tx -channelID Hospital2channel -asOrg Hospital2MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for Hospital1MSP..."
   exit 1
@@ -85,20 +85,20 @@ fi
 # Hospital1_3_Channel generation
 #------------------------------------------------------------------------------------------
 # generate channel configuration transaction
-configtxgen -profile Hospital1_3_channel -outputCreateChannelTx ./channel-artifacts/hospital1_3_channel.tx -channelID Hospital1_3_channel
+configtxgen -profile Hospital3channel -outputCreateChannelTx ./channel-artifacts/hospital1_3_channel.tx -channelID Hospital3channel
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate channel configuration transaction..."
   exit 1
 fi
 # generate anchor peer transaction
-configtxgen -profile Hospital1_3_channel -outputAnchorPeersUpdate ./channel-artifacts/Hospital1MSPanchors.tx -channelID Hospital1_3_channel -asOrg Hospital1MSP
+configtxgen -profile Hospital3channel -outputAnchorPeersUpdate ./channel-artifacts/Hospital1_3MSPanchors.tx -channelID Hospital3channel -asOrg Hospital1MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for Hospital1MSP..."
   exit 1
 fi
 
 # generate anchor peer transaction
-configtxgen -profile Hospital1_3_channel -outputAnchorPeersUpdate ./channel-artifacts/Hospital3MSPanchors.tx -channelID Hospital1_3_channel -asOrg Hospital3MSP
+configtxgen -profile Hospital3channel -outputAnchorPeersUpdate ./channel-artifacts/Hospital3_1MSPanchors.tx -channelID Hospital3channel -asOrg Hospital3MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for Hospital1MSP..."
   exit 1
